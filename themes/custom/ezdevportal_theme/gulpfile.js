acf2e4ffc0b5ea10b8e083ce976750c6130d36fa
anilivatury <anil.ivatury@srijan.net>
2023-01-23 13:48:54 +0530

    color feature suggenstions

--- themes/custom/ezdevportal_theme/gulpfile.js
+++ themes/custom/ezdevportal_theme/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 var cleanCSS = require('gulp-clean-css');
 var sassGlob = require('gulp-sass-glob');
 var browserSync = require('browser-sync').create();
