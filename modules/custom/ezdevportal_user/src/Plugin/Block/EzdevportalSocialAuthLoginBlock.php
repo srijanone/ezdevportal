4cd951d07f35fe0c4a5c3cab9a4d35474551c7d9
Arisen Fernandes <arisen.fernandes@materialplus.io>
2023-08-25 18:19:48 +0530

    social auth fixes

--- modules/custom/ezdevportal_user/src/Plugin/Block/EzdevportalSocialAuthLoginBlock.php
+++ modules/custom/ezdevportal_user/src/Plugin/Block/EzdevportalSocialAuthLoginBlock.php
@@ -3,7 +3,6 @@
 namespace Drupal\ezdevportal_user\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\social_api\Plugin\NetworkManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -27,13 +26,6 @@ class EzdevportalSocialAuthLoginBlock extends BlockBase implements ContainerFact
   private $networkManager;
 
   /**
-   * Immutable configuration for social_auth.settings.
-   *
-   * @var \Drupal\Core\Config\ImmutableConfig
-   */
-  private $socialAuthConfig;
-
-  /**
    * SocialAuthLoginBlock constructor.
    *
    * @param array $configuration
@@ -42,8 +34,6 @@ class EzdevportalSocialAuthLoginBlock extends BlockBase implements ContainerFact
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\Core\Config\ImmutableConfig $social_auth_config
-   *   The Immutable configuration for social_oauth.settings.
    * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
    *   The Social API network manager.
    */
@@ -51,12 +41,10 @@ class EzdevportalSocialAuthLoginBlock extends BlockBase implements ContainerFact
     array $configuration,
     string $plugin_id,
     $plugin_definition,
-    ImmutableConfig $social_auth_config,
     NetworkManager $network_manager
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->networkManager = $network_manager;
-    $this->socialAuthConfig = $social_auth_config;
   }
 
   /**
@@ -67,7 +55,6 @@ class EzdevportalSocialAuthLoginBlock extends BlockBase implements ContainerFact
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('config.factory')->get('social_auth.settings'),
       $container->get('plugin.network.manager')
     );
   }
@@ -76,21 +63,13 @@ class EzdevportalSocialAuthLoginBlock extends BlockBase implements ContainerFact
    * {@inheritdoc}
    */
   public function build() {
-
-    $networks = $this->networkManager->getDefinitions();
-    $social_networks = $this->socialAuthConfig->get('auth');
-    foreach ($social_networks as $id => &$social_network) {
-      $social_network['name'] = NULL;
-      if (isset($networks[$id]) && isset($networks[$id]['social_network'])) {
-        $social_network['name'] = $networks[$id]['social_network'];
-      }
-      // @todo get image path from custom settings defined in this module
-      // $social_network['img_path'] = '';
+    $networks = [];
+    foreach ($this->networkManager->getDefinitions() as $definition) {
+      $networks[] = $this->networkManager->createInstance($definition['id']);
     }
     return [
       '#theme' => 'ezdevportal_login_with',
-      '#social_networks' => $social_networks,
+      '#social_networks' => $networks,
     ];
   }
-
 }
